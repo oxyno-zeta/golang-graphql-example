5822aaa464013b6adecbc15f12ae80efbb751292
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-09-13 21:52:34 +0200

    feat: Upgrade and pin tools versions

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pip3 install pre-commit==2.7.1
+pip3 install pre-commit==2.15.0
 
 pre-commit install
 pre-commit install --hook-type commit-msg
 
-yarn global add @graphql-inspector/graphql-loader @graphql-inspector/git-loader @graphql-inspector/diff-command @graphql-inspector/cli graphql
+yarn global add @graphql-inspector/graphql-loader@2.9.0 @graphql-inspector/git-loader@2.9.0 @graphql-inspector/diff-command@2.9.0 @graphql-inspector/cli@2.9.0 graphql@15.5.3
