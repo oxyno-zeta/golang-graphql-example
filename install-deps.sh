704092ec211cf0df3641a29cca56fa4a3ceebcc0
Havrileck Alexandre <havrileckalexandre@gmail.com>
2022-05-03 22:09:47 +0200

    feat: Update core dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pip3 install pre-commit==2.15.0
+pip3 install pre-commit==2.18.1
 
 pre-commit install
 pre-commit install --hook-type commit-msg
 
-yarn global add @graphql-inspector/graphql-loader@2.9.0 @graphql-inspector/git-loader@2.9.0 @graphql-inspector/diff-command@2.9.0 @graphql-inspector/cli@2.9.0 graphql@15.5.3
+yarn global add @graphql-inspector/graphql-loader@3.1.2 @graphql-inspector/git-loader@3.1.2 @graphql-inspector/diff-command@3.1.2 @graphql-inspector/cli@3.1.2 graphql@16.4.0
