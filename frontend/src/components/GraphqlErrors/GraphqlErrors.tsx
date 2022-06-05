91bdde882107137aa80f7788b0d54e01cc5efc23
Havrileck Alexandre <havrileckalexandre@gmail.com>
2022-06-05 14:51:54 +0200

    feat(frontend:components): Allow to remove the margin on GraphQL errors component

--- frontend/src/components/GraphqlErrors/GraphqlErrors.tsx
+++ frontend/src/components/GraphqlErrors/GraphqlErrors.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Typography from '@mui/material//Typography';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material//Box';
 import { ApolloError, ServerError } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
 
 interface Props {
   error: ApolloError;
+  noMargin?: boolean;
 }
 
 interface CustomNetworkError {
@@ -14,12 +15,12 @@ interface CustomNetworkError {
 }
 
 /* eslint-disable react/no-array-index-key */
-function GraphqlErrors({ error }: Props) {
+function GraphqlErrors({ error, noMargin }: Props) {
   // Initialize translate
   const { t } = useTranslation();
 
   return (
-    <Box sx={{ color: 'error', margin: 8 }}>
+    <Box sx={{ color: 'error', margin: noMargin ? 0 : 8 }}>
       <Typography color="error">{t('common.errors')}:</Typography>
       <ul>
         {error.networkError && (!(error.networkError as ServerError).result || !error.graphQLErrors) && (
@@ -48,5 +49,9 @@ function GraphqlErrors({ error }: Props) {
   );
 }
 /* eslint-enable */
+
+GraphqlErrors.defaultProps = {
+  noMargin: false,
+};
 
 export default GraphqlErrors;
