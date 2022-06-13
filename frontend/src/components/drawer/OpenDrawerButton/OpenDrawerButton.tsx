1d04bce02bf0f969d9eedec0997c74cdacd5c3e2
Havrileck Alexandre <havrileckalexandre@gmail.com>
2022-06-13 17:05:33 +0200

    feat(ui): Add Tooltip message on open drawer button

--- frontend/src/components/drawer/OpenDrawerButton/OpenDrawerButton.tsx
+++ frontend/src/components/drawer/OpenDrawerButton/OpenDrawerButton.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Tooltip from '@mui/material/Tooltip';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   handleDrawerToggle: () => void;
 }
 
 function OpenDrawerButton({ handleDrawerToggle }: Props) {
+  // Setup translate
+  const { t } = useTranslation();
+
   return (
-    <IconButton color="inherit" onClick={handleDrawerToggle} sx={{ display: { sm: 'none' } }}>
-      <MenuIcon />
-    </IconButton>
+    <Tooltip title={<>{t('common.openAction')}</>}>
+      <span>
+        <IconButton color="inherit" onClick={handleDrawerToggle} sx={{ display: { sm: 'none' } }}>
+          <MenuIcon />
+        </IconButton>
+      </span>
+    </Tooltip>
   );
 }
 
