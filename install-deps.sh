ebd8a5173290892b5009fb4159e0f5887d278ad2
Havrileck Alexandre <havrileckalexandre@gmail.com>
2022-08-05 23:02:04 +0200

    feat: Upgrade pre-commit and graphql-inspector

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pip3 install pre-commit==2.18.1
+pip3 install pre-commit==2.20.0
 
 pre-commit install
 pre-commit install --hook-type commit-msg
 
-yarn global add @graphql-inspector/graphql-loader@3.1.2 @graphql-inspector/git-loader@3.1.2 @graphql-inspector/diff-command@3.1.2 @graphql-inspector/cli@3.1.2 graphql@16.4.0
+yarn global add @graphql-inspector/graphql-loader@3.3.0 @graphql-inspector/git-loader@3.3.0 @graphql-inspector/diff-command@3.3.0 @graphql-inspector/cli@3.3.0 graphql@16.5.0
