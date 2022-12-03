5d82f0e66b908d733d09cc153e614059f4e9d955
Havrileck Alexandre <havrileckalexandre@gmail.com>
2022-12-03 12:00:57 +0100

    feat(backend:log): Add right support for SQL logs with all context fields

--- backend/pkg/golang-graphql-example/log/gorm-logger.go
+++ backend/pkg/golang-graphql-example/log/gorm-logger.go
@@ -16,25 +16,37 @@ func (gl *gormLogger) LogMode(gormlogger.LogLevel) gormlogger.Interface {
 	return gl
 }
 
+func (gl *gormLogger) getCtxLoggerOrDefault(ctx context.Context) Logger {
+	// Get logger from context
+	ctxL := GetLoggerFromContext(ctx)
+	// Check if it exists
+	if ctxL != nil {
+		return ctxL
+	}
+
+	// Default
+	return gl.logger
+}
+
 func (gl *gormLogger) Info(ctx context.Context, v string, rest ...interface{}) {
 	val := []interface{}{v}
 	val = append(val, rest...)
-	gl.logger.Info(val...)
+	gl.getCtxLoggerOrDefault(ctx).Info(val...)
 }
 
 func (gl *gormLogger) Warn(ctx context.Context, v string, rest ...interface{}) {
 	val := []interface{}{v}
 	val = append(val, rest...)
-	gl.logger.Warn(val...)
+	gl.getCtxLoggerOrDefault(ctx).Warn(val...)
 }
 
 func (gl *gormLogger) Error(ctx context.Context, v string, rest ...interface{}) {
 	val := []interface{}{errors.New(v)}
 	val = append(val, rest...)
-	gl.logger.Error(val...)
+	gl.getCtxLoggerOrDefault(ctx).Error(val...)
 }
 
 func (gl *gormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	sql, rows := fc()
-	gl.logger.WithField("rows", rows).WithError(err).Debug(sql)
+	gl.getCtxLoggerOrDefault(ctx).WithField("rows", rows).WithError(err).Debug(sql)
 }
