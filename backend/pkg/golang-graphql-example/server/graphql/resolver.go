9a770a570a87dd1ed97dcc5b5e12e1a35de25b04
Havrileck Alexandre <oxyno-zeta@users.noreply.github.com>
2023-05-09 12:14:56 +0200

    fix(backend:server:graphql): Remove unneeded go generate command

--- backend/pkg/golang-graphql-example/server/graphql/resolver.go
+++ backend/pkg/golang-graphql-example/server/graphql/resolver.go
@@ -2,8 +2,6 @@ package graphql
 
 import "github.com/oxyno-zeta/golang-graphql-example/pkg/golang-graphql-example/business"
 
-//go:generate gqlgen generate
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
