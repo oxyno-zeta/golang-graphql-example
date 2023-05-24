2ebd9f5e2be93bfcd1e008834d8c36af8e52b294
Havrileck Alexandre <havrileckalexandre@gmail.com>
2023-05-24 22:19:02 +0200

    fix(frontend:components:TopBar): Fix size and fix in flex

--- frontend/src/components/TopBar/TopBarSpacer.tsx
+++ frontend/src/components/TopBar/TopBarSpacer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 function TopBarSpacer() {
+  // First div is for upper div in flex
   return (
-    <div
-      style={{
-        height: '43px', // This size is the app bar dense mode size. This isn't present in the theme so...
-      }}
-    />
+    <div>
+      <div
+        style={{
+          height: '48px', // This size is the app bar dense mode size. This isn't present in the theme so...
+        }}
+      />
+    </div>
   );
 }
 
