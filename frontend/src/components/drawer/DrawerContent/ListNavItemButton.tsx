66f04a9f055ca530991d422e7e25ec4498d3d29f
Havrileck Alexandre <havrileckalexandre@gmail.com>
2023-05-29 22:11:00 +0200

    feat(frontend:components:drawer:DrawerContent): Add listItemButtonProps to add more possibilities

--- frontend/src/components/drawer/DrawerContent/ListNavItemButton.tsx
+++ frontend/src/components/drawer/DrawerContent/ListNavItemButton.tsx
@@ -1,20 +1,30 @@
 import React, { ReactNode } from 'react';
 import { Link as RouterLink, useMatch, useResolvedPath } from 'react-router-dom';
-import ListItemButton from '@mui/material/ListItemButton';
+import ListItemButton, { ListItemButtonProps } from '@mui/material/ListItemButton';
 
 interface Props {
   to: string;
   children: ReactNode;
+  listItemButtonProps?: Omit<ListItemButtonProps, 'component' | 'to' | 'selected'>;
 }
 
-function ListNavItemButton({ to, children }: Props) {
+const defaultProps = {
+  listItemButtonProps: {},
+};
+
+function ListNavItemButton({ to, children, listItemButtonProps }: Props) {
   const match = useMatch({ path: useResolvedPath(to).pathname, end: true });
 
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  // Coming from official mui documentation
+  // https://mui.com/material-ui/react-breadcrumbs/#integration-with-react-router
   return (
-    <ListItemButton component={RouterLink} to={to} selected={!!match}>
+    <ListItemButton component={RouterLink as any} to={to} selected={!!match} {...listItemButtonProps}>
       {children}
     </ListItemButton>
   );
 }
+
+ListNavItemButton.defaultProps = defaultProps;
 
 export default ListNavItemButton;
