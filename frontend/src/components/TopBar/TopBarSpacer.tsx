085fdeed982a390dbb49bb5a573c6d4f9528007c
Havrileck Alexandre <havrileckalexandre@gmail.com>
2023-05-29 21:31:35 +0200

    fix(frontend:components:TopBar): Fix TopBarSpacer to be more aligned with TopBar

--- frontend/src/components/TopBar/TopBarSpacer.tsx
+++ frontend/src/components/TopBar/TopBarSpacer.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import Toolbar from '@mui/material/Toolbar';
 
 function TopBarSpacer() {
   // First div is for upper div in flex
-  return (
-    <div>
-      <div
-        style={{
-          height: '48px', // This size is the app bar dense mode size. This isn't present in the theme so...
-        }}
-      />
-    </div>
-  );
+  return <Toolbar variant="dense" />;
 }
 
 export default TopBarSpacer;
