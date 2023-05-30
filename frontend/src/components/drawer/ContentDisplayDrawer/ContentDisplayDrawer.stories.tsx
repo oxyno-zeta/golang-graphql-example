8c0dd8d79f405ab374e755fc534cd2532fde96f7
Havrileck Alexandre <havrileckalexandre@gmail.com>
2023-05-30 23:47:53 +0200

    feat(frontend:components:drawer:ContentDisplayDrawer): Rework to make it resizable and with a better interface

---
+++ frontend/src/components/drawer/ContentDisplayDrawer/ContentDisplayDrawer.stories.tsx
@@ -0,0 +1,177 @@
+import React, { ReactNode } from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
+import * as dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import TimezoneProvider from '~components/timezone/TimezoneProvider';
+import TopBar, { TopBarSpacer } from '~components/TopBar';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SvgIcon from '@mui/material/SvgIcon';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { mdiAccessPoint } from '@mdi/js';
+import ConfigContext from '../../../contexts/ConfigContext';
+import { defaultConfig } from '../../../models/config';
+import ContentDisplayDrawer, { Props } from './ContentDisplayDrawer';
+import ListNavItemButton from '../ListNavItemButton';
+import PageDrawer from '../PageDrawer';
+
+// Extend dayjs
+dayjs.extend(localizedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export default {
+  title: 'Components/drawer/ContentDisplayDrawer',
+  component: ContentDisplayDrawer,
+  args: {
+    defaultDrawerWidth: 200,
+    drawerElement: <div>Content !</div>,
+  },
+  decorators: [withRouter],
+} as Meta<typeof ContentDisplayDrawer>;
+
+function RemoveStorybookPadding({ children }: { children: ReactNode }) {
+  return <div style={{ margin: '-1rem' }}>{children}</div>;
+}
+
+function Content() {
+  return (
+    <div style={{ margin: '10px' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Paper>xs=8</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>xs=4</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>xs=4</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper>xs=8</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export const DisableTopSpace: StoryFn<typeof ContentDisplayDrawer> = function C({ disableTopSpacer, ...args }: Props) {
+  return (
+    <RemoveStorybookPadding>
+      <ContentDisplayDrawer disableTopSpacer {...args}>
+        <Content />
+      </ContentDisplayDrawer>
+    </RemoveStorybookPadding>
+  );
+};
+
+export const DisableResize: StoryFn<typeof ContentDisplayDrawer> = function C({ disableResize, ...args }: Props) {
+  return (
+    <RemoveStorybookPadding>
+      <ContentDisplayDrawer disableResize {...args}>
+        <Content />
+      </ContentDisplayDrawer>
+    </RemoveStorybookPadding>
+  );
+};
+
+export const WithTopBarOnly: StoryFn<typeof ContentDisplayDrawer> = function C({ ...args }: Props) {
+  return (
+    <RemoveStorybookPadding>
+      <TimezoneProvider>
+        <ConfigContext.Provider value={defaultConfig}>
+          <TopBar />
+          <ContentDisplayDrawer {...args}>
+            <TopBarSpacer />
+            <Content />
+          </ContentDisplayDrawer>
+        </ConfigContext.Provider>
+      </TimezoneProvider>
+    </RemoveStorybookPadding>
+  );
+};
+
+export const WithTopBarAndRightMenu: StoryFn<typeof ContentDisplayDrawer> = function C({ ...args }: Props) {
+  return (
+    <RemoveStorybookPadding>
+      <TimezoneProvider>
+        <ConfigContext.Provider value={defaultConfig}>
+          <TopBar />
+          <PageDrawer
+            defaultDrawerWidth={200}
+            renderDrawerContent={({ listItemButtonSx, listItemIconSx, listItemTextSx }) => (
+              <>
+                <ListItem disablePadding>
+                  <ListItemButton dense sx={listItemButtonSx}>
+                    <ListItemIcon sx={listItemIconSx}>
+                      <SvgIcon>
+                        <path d={mdiAccessPoint} />
+                      </SvgIcon>
+                    </ListItemIcon>
+                    <ListItemText
+                      primary="Menu"
+                      secondary="Sub menu"
+                      secondaryTypographyProps={{ noWrap: true, fontSize: 12, lineHeight: '16px' }}
+                      sx={listItemTextSx}
+                    />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding dense>
+                  <ListItemButton dense sx={listItemButtonSx}>
+                    <ListItemIcon sx={listItemIconSx}>
+                      <SvgIcon>
+                        <path d={mdiAccessPoint} />
+                      </SvgIcon>
+                    </ListItemIcon>
+                    <ListItemText sx={listItemTextSx}>Menu</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding dense>
+                  <ListItemButton dense sx={listItemButtonSx}>
+                    <ListItemIcon sx={listItemIconSx}>
+                      <SvgIcon>
+                        <path d={mdiAccessPoint} />
+                      </SvgIcon>
+                    </ListItemIcon>
+                    <ListItemText sx={listItemTextSx}>Menu</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding dense>
+                  <ListItemButton dense sx={listItemButtonSx}>
+                    <ListItemIcon sx={listItemIconSx}>
+                      <SvgIcon>
+                        <path d={mdiAccessPoint} />
+                      </SvgIcon>
+                    </ListItemIcon>
+                    <ListItemText sx={listItemTextSx}>Menu</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListNavItemButton to="/fail">
+                    <ListItemIcon sx={listItemIconSx}>
+                      <SvgIcon>
+                        <path d={mdiAccessPoint} />
+                      </SvgIcon>
+                    </ListItemIcon>
+                    <ListItemText sx={listItemTextSx}>Link</ListItemText>
+                  </ListNavItemButton>
+                </ListItem>
+              </>
+            )}
+            renderContent={() => (
+              <ContentDisplayDrawer {...args}>
+                <Content />
+              </ContentDisplayDrawer>
+            )}
+          />
+        </ConfigContext.Provider>
+      </TimezoneProvider>
+    </RemoveStorybookPadding>
+  );
+};
--- frontend/src/components/drawer/ContentDisplayDrawer/ContentDisplayDrawer.tsx
+++ frontend/src/components/drawer/ContentDisplayDrawer/ContentDisplayDrawer.tsx
@@ -1,80 +1,181 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useCallback, useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer, { DrawerProps } from '@mui/material/Drawer';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import SvgIcon from '@mui/material/SvgIcon';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import { useTheme } from '@mui/material/styles';
-import type { SxProps } from '@mui/material';
+import type { Theme } from '@mui/material';
+import type { SystemStyleObject } from '@mui/system';
+import { mdiClose } from '@mdi/js';
+import { useTranslation } from 'react-i18next';
+import { TopBarSpacer } from '~components/TopBar';
 
 export interface Props {
-  drawerWidth: number | string;
+  // Default drawer width for init.
+  defaultDrawerWidth: number;
   drawerElement: ReactNode;
-  onMobileDrawerToggle: () => void;
+  children: ReactNode;
+  onClose: () => void;
   mobileDrawerProps?: Partial<Omit<DrawerProps, 'open' | 'onClose'>>;
   drawerProps?: Partial<Omit<DrawerProps, 'open'>>;
-  drawerContainerBoxSx?: SxProps;
+  mainContainerBoxSx?: Omit<SystemStyleObject<Theme>, 'width'>;
+  drawerContainerBoxSx?: Omit<SystemStyleObject<Theme>, 'width' | 'display' | 'flexShrink'>;
+  disableTopSpacer?: boolean;
+  titleElement?: ReactNode;
+  minDrawerWidth?: number;
+  maxDrawerWidth?: number;
+  disableResize?: boolean;
 }
+
+const defaultMinDrawerWidth = 150;
+const defaultMaxDrawerWidth = 400;
 
 const defaultProps = {
   mobileDrawerProps: {},
   drawerProps: {},
   drawerContainerBoxSx: {},
+  mainContainerBoxSx: {},
+  disableTopSpacer: false,
+  titleElement: null,
+  minDrawerWidth: defaultMinDrawerWidth,
+  maxDrawerWidth: defaultMaxDrawerWidth,
+  disableResize: false,
 };
 
 function ContentDisplayDrawer({
+  children,
   drawerElement,
-  drawerWidth,
-  onMobileDrawerToggle,
+  defaultDrawerWidth,
+  onClose,
   mobileDrawerProps,
   drawerProps,
   drawerContainerBoxSx,
+  mainContainerBoxSx,
+  disableTopSpacer,
+  titleElement,
+  minDrawerWidth = defaultMinDrawerWidth,
+  maxDrawerWidth = defaultMaxDrawerWidth,
+  disableResize,
 }: Props) {
+  // Setup translate
+  const { t } = useTranslation();
+  // State
+  const [drawerWidth, setDrawerWidth] = useState(defaultDrawerWidth);
+  // Variables
   const open = Boolean(drawerElement);
   const theme = useTheme();
   const sizeMatching = useMediaQuery(theme.breakpoints.up('lg'));
 
+  const handleMouseMove = useCallback((e: MouseEvent) => {
+    const newWidth = document.body.offsetWidth - (e.clientX - document.body.offsetLeft);
+
+    if (newWidth > minDrawerWidth && newWidth < maxDrawerWidth) {
+      setDrawerWidth(newWidth);
+    }
+  }, []);
+
+  function handleMouseDown() {
+    document.addEventListener('mouseup', handleMouseUp, true);
+    document.addEventListener('mousemove', handleMouseMove, true);
+  }
+
+  function handleMouseUp() {
+    document.removeEventListener('mouseup', handleMouseUp, true);
+    document.removeEventListener('mousemove', handleMouseMove, true);
+  }
+
+  const content = (
+    <>
+      {!disableResize && (
+        <Divider
+          role="button"
+          onMouseDown={() => handleMouseDown()}
+          orientation="vertical"
+          flexItem
+          sx={(th) => ({
+            cursor: 'ew-resize',
+            position: 'absolute',
+            borderRightWidth: '1px',
+            backgroundColor: 'unset',
+            '&:hover': { borderWidth: '2px', backgroundColor: th.palette.divider },
+            width: 0,
+            top: 0,
+            left: 0,
+            bottom: 0,
+          })}
+        />
+      )}
+      <div style={{ display: 'flex', alignItems: 'center', margin: '10px' }}>
+        {titleElement || (
+          <Typography variant="h6" sx={{ marginRight: 'auto' }}>
+            {t('common.details')}
+          </Typography>
+        )}
+        <Tooltip title={<>{t('common.closeAction')}</>}>
+          <span>
+            <IconButton onClick={onClose} sx={{ marginLeft: 'auto' }}>
+              <SvgIcon>
+                <path d={mdiClose} />
+              </SvgIcon>
+            </IconButton>
+          </span>
+        </Tooltip>
+      </div>
+      {drawerElement}
+    </>
+  );
+
   return (
-    <Box
-      sx={{
-        display: open ? 'block' : 'none',
-        width: { lg: drawerWidth },
-        flexShrink: { lg: 0 },
-        ...drawerContainerBoxSx,
-      }}
-    >
-      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-      {open && !sizeMatching && (
+    <Box sx={{ marginRight: `${drawerWidth}px`, ...mainContainerBoxSx }}>
+      {children}
+      <Box
+        sx={{
+          display: open ? 'block' : 'none',
+          width: { lg: `${drawerWidth}px` },
+          flexShrink: { lg: 0 },
+          ...drawerContainerBoxSx,
+        }}
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        {open && !sizeMatching && (
+          <Drawer
+            variant="temporary"
+            open={open}
+            onClose={onClose}
+            anchor="right"
+            sx={{
+              display: 'block',
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
+            {...mobileDrawerProps}
+          >
+            {content}
+          </Drawer>
+        )}
         <Drawer
-          variant="temporary"
-          open={open}
-          onClose={onMobileDrawerToggle}
-          anchor="right"
+          variant="persistent"
           sx={{
-            display: 'block',
+            display: { xs: 'none', lg: 'block' },
             '& .MuiDrawer-paper': {
               boxSizing: 'border-box',
               width: drawerWidth,
             },
           }}
-          {...mobileDrawerProps}
+          anchor="right"
+          open={open}
+          {...drawerProps}
         >
-          {drawerElement}
+          {!disableTopSpacer && <TopBarSpacer />}
+          {content}
         </Drawer>
-      )}
-      <Drawer
-        variant="persistent"
-        sx={{
-          display: { xs: 'none', lg: 'block' },
-          '& .MuiDrawer-paper': {
-            boxSizing: 'border-box',
-            width: drawerWidth,
-          },
-        }}
-        anchor="right"
-        open={open}
-        {...drawerProps}
-      >
-        {drawerElement}
-      </Drawer>
+      </Box>
     </Box>
   );
 }
