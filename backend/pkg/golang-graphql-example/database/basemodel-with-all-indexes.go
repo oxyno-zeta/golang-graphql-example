35c587ea68dab6bb7ba55c45d4e30233d8f7cbdb
Havrileck Alexandre <havrileckalexandre@gmail.com>
2024-02-05 23:21:01 +0100

    feat(backend:database): Add base with all indexes on all dates

---
+++ backend/pkg/golang-graphql-example/database/basemodel-with-all-indexes.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"time"
+
+	"emperror.dev/errors"
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+)
+
+// Base contains common columns for all tables.
+type BaseWithAllIndexes struct {
+	CreatedAt time.Time  `gorm:"index"`
+	UpdatedAt time.Time  `gorm:"index"`
+	DeletedAt *time.Time `gorm:"index"`
+	ID        string     `gorm:"primary_key"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (base *BaseWithAllIndexes) BeforeCreate(_ *gorm.DB) error {
+	// Check if ID is set to avoid erasing it.
+	// This is useful when it is asked to save object for the first
+	// time with a fixed id.
+	if base.ID == "" {
+		// Generate new id
+		uuidGenerated, err := uuid.NewV7()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// Save new id
+		base.ID = uuidGenerated.String()
+	}
+
+	return nil
+}
