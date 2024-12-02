fd2b4724de9536bd0c74a727dab61c1e29cb89d0
Havrileck Alexandre <havrileckalexandre@gmail.com>
2024-12-02 23:47:36 +0100

    feat(frontend:components:form:FormAutocomplete): Improve error management

--- frontend/src/components/form/FormAutocomplete/FormAutocomplete.tsx
+++ frontend/src/components/form/FormAutocomplete/FormAutocomplete.tsx
@@ -39,15 +39,21 @@ function FormAutocomplete<T extends FieldValues>({
     // Set error boolean
     errorProps.error = !!fieldState.error;
 
-    // Check type <of error message
+    // Check type of error message
     if (typeof fieldState.error.message === 'object') {
       const mess = fieldState.error.message as YupTranslateErrorModel;
 
       // Add helper text
-      errorProps.helperText = <>{t(mess.key, mess.values)}</>;
+      errorProps.helperText = t(mess.key, mess.values) as string;
     } else if (typeof fieldState.error.message === 'string') {
       // Add helper text
       errorProps.helperText = t(fieldState.error.message);
+    } else if (Array.isArray(fieldState.error)) {
+      errorProps.helperText = fieldState.error.reduce((acc, v) => {
+        const mess = v.message as YupTranslateErrorModel;
+
+        return `${acc} ${t(mess.key, mess.values)}` as string;
+      }, '');
     }
   }
 
