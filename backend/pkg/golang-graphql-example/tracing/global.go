20731bc15f62212f871a5d8640201517b399428a
Havrileck Alexandre <havrileckalexandre@gmail.com>
2025-06-07 12:07:55 +0200

    style(backend): Rewrite a nolint comment

--- backend/pkg/golang-graphql-example/tracing/global.go
+++ backend/pkg/golang-graphql-example/tracing/global.go
@@ -86,7 +86,8 @@ func manageGenericAttribute(key string, value interface{}) *attribute.KeyValue {
 
 	// Parse
 	aValue := reflect.ValueOf(value)
-	switch aValue.Kind() { //nolint:exhaustive // That's why "default" is present...
+	//nolint:exhaustive // That's why "default" is present...
+	switch aValue.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		res = attribute.Int64(key, aValue.Int())
 	case reflect.Float32, reflect.Float64:
